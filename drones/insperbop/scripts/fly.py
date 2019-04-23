1341c907d80fb91c9b7bba2e7455017c46dea3c9
mirwox <fmiranda@gmail.com>
2019-04-23 15:06:44 -0300

    script para voar

--- drones/insperbop/scripts/fly.py
+++ drones/insperbop/scripts/fly.py
@@ -6,28 +6,35 @@ import rospy
 
 from geometry_msgs.msg import Twist, Vector3
 
-v = 10  # Velocidade linear
+from std_msgs.msg import Empty
+
+empty_msg = Empty()
+
 w = 0  # Velocidade angular
 
-pub = rospy.Publisher("bebop/cmd_vel", Twist, queue_size = 1)
-takeoff = rospy.Publisher('bebop/takeoff', Empty, queue_size = 1)
-landing = rospy.Publisher('bebop/land', Empty, queue_size = 1)
-zerov = Twist(Vector3(0,0,0), Vector3(0,0,0))
-vel = Twist(Vector3(v,0,0), Vector3(0,0,w))
+
 
 
 
 if __name__ == "__main__":
     rospy.init_node("fly")
-    pub = rospy.Publisher("cmd_vel", Twist, queue_size=3)
+    pub = rospy.Publisher("/bebop/cmd_vel", Twist, queue_size=1)
+    takeoff = rospy.Publisher('bebop/takeoff', Empty, queue_size = 1, latch=True)
+    landing = rospy.Publisher('bebop/land', Empty, queue_size = 1, latch=True)
+    zerov = Twist(Vector3(0,0,0), Vector3(0,0,0))
 
-    count = 5
+    v = 0.8  # Velocidade linear
+    vel = Twist(Vector3(v,0,0), Vector3(0,0,0))
+
+
+    count = 15
 
     try:
         while count > 0:
             pub.publish(vel)
-            rospy.sleep(1.0)
+            rospy.sleep(0.3)
             count -= 1
+            print(count)
         pub.publish(zerov)
         rospy.sleep(2.0)
     except rospy.ROSInterruptException:
