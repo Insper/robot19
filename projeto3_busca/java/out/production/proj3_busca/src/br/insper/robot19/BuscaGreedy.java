ce37cfd6cbbab7461db1588375699ceafafa5a6c
Pedro Daher <pedrohenriquedaher@icloud.com>
2019-05-24 20:21:27 -0300

    AStar Implemented

---
+++ projeto3_busca/java/out/production/proj3_busca/src/br/insper/robot19/BuscaGreedy.java
@@ -0,0 +1,95 @@
+package br.insper.robot19;
+
+import java.util.*;
+
+public class BuscaGreedy {
+
+    private Block start = null;
+    private Block end = null;
+    private GridMap map = null;
+
+    private Queue<Node> border;
+
+    /**
+     * Construtor
+     * @param map
+     * @param start - ponto inicial
+     * @param end - ponto final
+     */
+    public BuscaGreedy(GridMap map, Block start, Block end) {
+        this.map = map;
+        this.start = start;
+        this.end = end;
+    }
+
+    /**
+     * Método que realiza a busca
+     * @return
+     */
+    public Node buscar() {
+
+        HashSet<Node> stack = new HashSet<>();
+
+        Node root = new Node(start, null, null, 0);
+
+        //Limpa a fronteira e insere o nó raiz
+
+        Comparator<Node> comparator = new ComparadorGreedy();
+        border = new PriorityQueue<Node>(comparator);
+        border.add(root);
+
+
+        while(!border.isEmpty()) {
+
+            Node node = border.remove();
+            Block atual = node.getValue();
+
+            if(atual.row == end.row && atual.col == end.col) {
+                return node;
+
+            } else for(RobotAction acao : RobotAction.values()) {
+
+                Block proximo = map.nextBlock(atual, acao);
+
+                if(proximo != null && proximo.type != BlockType.WALL) {
+
+                    Node novoNode = new Node(proximo, node, acao, proximo.type.cost);
+                    novoNode.setH((Math.abs(end.col - atual.col) + Math.abs( end.row - atual.row)));
+
+                    if (!stack.contains(novoNode)){
+
+                        border.add(novoNode);
+                        stack.add(novoNode);
+
+                    }
+                }
+            }
+        }
+        return null;
+    }
+
+    /**
+     * Resolve o problema com base em busca, realizando
+     * o backtracking após chegar ao estado final
+     *
+     * @return A solução encontrada
+     */
+    public RobotAction[] resolver() {
+
+        // Encontra a solução através da busca
+        Node destino = buscar();
+        if(destino == null) {
+            return null;
+        }
+
+        //Faz o backtracking para recuperar o caminho percorrido
+        Node atual = destino;
+        Deque<RobotAction> caminho = new LinkedList<RobotAction>();
+        while(atual.getAction() != null) {
+            caminho.addFirst(atual.getAction());
+            atual = atual.getParent();
+        }
+        return caminho.toArray(new RobotAction[caminho.size()]);
+    }
+}
+
---
+++ projeto3_busca/java/out/production/proj3_busca/src/br/insper/robot19/GraficoMapGreedy.java
@@ -0,0 +1,163 @@
+package br.insper.robot19;
+
+import java.awt.*;
+import java.io.IOException;
+
+public class GraficoMapGreedy {
+    GridMap map;
+    int width;
+    int height;
+    Canvas screen;
+    //BufferedImage imagem;
+
+    /**
+     *
+     * @param g Um objeto do itpo GridMap
+     */
+    public GraficoMapGreedy (GridMap g){
+        this(g, 800, 600    );
+    }
+
+    /**
+     *
+     * @param g Um objeto do tipo GridMap
+     * @param width A largura desejada para a janela
+     * @param height A altura desejada para a janela
+     */
+    public GraficoMapGreedy(GridMap g, int width, int height){
+        this.map = g;
+        this.width = width;
+        this.height = height;
+        screen = new Canvas("Proj 3 Busca", this.width, this.height, Color.lightGray);
+
+    }
+
+    /**
+     * Desenha uma representação gráfica do map que foi passado
+     *
+     * Vocês vão precisar alterar esta classe para mostrar nós expandidos e rota
+     */
+    public void desenha(){
+        int h = map.getHeight();
+        int w = map.getWidth();
+
+        int sqx = this.width/w;
+        int sqy = this.height/h;
+
+
+        for (int i=0; i < h; i ++ ){
+            for (int j = 0; j < w; j++){
+
+
+                switch  (map.getBlockType(i, j)) {
+                    case FREE:
+                        screen.setForegroundColor(Color.WHITE);
+                        break;
+                    case WALL:
+                        screen.setForegroundColor(Color.BLACK);
+                        break;
+                    case SAND:
+                        screen.setForegroundColor(Color.YELLOW);
+                        break;
+                    case METAL:
+                        screen.setForegroundColor(Color.BLUE);
+                        break;
+                }
+
+                screen.fillRectangle(j*sqx, i*sqy, sqx-2, sqy-2);
+            }
+        }
+
+        int[] goal = map.getGoal();
+        int[] start = map.getStart();
+
+        screen.setForegroundColor(Color.GREEN);
+        screen.fillCircle(start[1]*sqx+sqx/4, start[0]*sqy+sqy/4, sqx/2);
+
+        screen.setForegroundColor(Color.RED);
+        screen.fillCircle(goal[1]*sqx+sqx/4, goal[0]*sqy+sqy/4, sqx/2);
+
+    }
+
+    /**
+     * Saves a png file with what's shown in the Canvas
+     */
+    public void saveFile(String filename){
+        screen.saveFile(filename);
+    }
+
+    /**
+     *
+     */
+    public void setAndDrawSolucao(RobotAction[] solucao){
+        if(solucao == null) {
+            System.out.println("Nao foi encontrada solucao para o problema");
+        } else {
+            int[] s = map.getStart();
+            Block atual = new Block(s[0], s[1], map.getBlockType(s[0], s[1]));
+            System.out.print("Solução: ");
+            for (RobotAction a : solucao) {
+                System.out.print(", " + a);
+                Block next = map.nextBlock(atual, a);
+                map.setRoute(next.row, next.col);
+                plotStep(atual, next);
+                atual = next;
+            }
+        }
+
+    }
+
+    /**
+     * Desenha uma linha entre dois blocos
+     * @param atual
+     * @param next
+     */
+    public void plotStep(Block atual, Block next){
+        int h = map.getHeight();
+        int w = map.getWidth();
+
+        int sqx = this.width/w;
+        int sqy = this.height/h;
+
+        screen.setForegroundColor(Color.GREEN);
+        screen.drawLine(atual.col*sqx + sqx/2, atual.row*sqy + sqy/2, next.col*sqx + sqx/2, next.row*sqy + sqy/2);
+
+    }
+
+    public static void main(String[] args) {
+        GridMap map = null;
+        try {
+            map = GridMap.fromFile("mapa");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        /**
+         * Situação: mostra o mapa inicial
+         */
+        GraficoMap grafico = new GraficoMap(map);
+        grafico.desenha();
+        grafico.saveFile("Busca1.png");
+
+        /**
+         * Agora vamos fazer uma busca em largura
+         */
+
+        // Bloco inicial
+        int s[] = map.getStart();
+        Block sb = new Block(s[0], s[1], map.getBlockType(s[0], s[1]));
+
+        // Bloco final
+        int f[] = map.getGoal();
+        Block g = new Block(f[0], f[1], map.getBlockType(f[0], f[1]));
+
+        // Rodando a busca
+        BuscaGreedy busca = new BuscaGreedy(map, sb, g);
+        RobotAction[] solucao = busca.resolver();
+
+        // Requisitando o plot da solução sobre o gráfico
+        grafico.setAndDrawSolucao(solucao);
+        grafico.saveFile("Resolvido.png");
+
+
+    }
+}
