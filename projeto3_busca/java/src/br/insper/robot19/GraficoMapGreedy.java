7e32ba99a49f4f50de0636c7103d499b0220f6e0
Aronakamoto <aronakamoto@outlook.com>
2019-05-23 22:00:42 -0300

    Greedy

--- projeto3_busca/java/src/br/insper/robot19/GraficoMapGreedy.java
+++ projeto3_busca/java/src/br/insper/robot19/GraficoMapGreedy.java
@@ -3,7 +3,7 @@ package br.insper.robot19;
 import java.awt.*;
 import java.io.IOException;
 
-public class GraficoMap {
+public class GraficoMapGreedy {
     GridMap map;
     int width;
     int height;
@@ -14,7 +14,7 @@ public class GraficoMap {
      *
      * @param g Um objeto do itpo GridMap
      */
-    public GraficoMap (GridMap g){
+    public GraficoMapGreedy (GridMap g){
         this(g, 800, 600    );
     }
 
@@ -24,7 +24,7 @@ public class GraficoMap {
      * @param width A largura desejada para a janela
      * @param height A altura desejada para a janela
      */
-    public GraficoMap(GridMap g, int width, int height){
+    public GraficoMapGreedy(GridMap g, int width, int height){
         this.map = g;
         this.width = width;
         this.height = height;
@@ -151,7 +151,7 @@ public class GraficoMap {
         Block g = new Block(f[0], f[1], map.getBlockType(f[0], f[1]));
 
         // Rodando a busca
-        BuscaLargura busca = new BuscaLargura(map, sb, g);
+        BuscaGreedy busca = new BuscaGreedy(map, sb, g);
         RobotAction[] solucao = busca.resolver();
 
         // Requisitando o plot da solução sobre o gráfico
