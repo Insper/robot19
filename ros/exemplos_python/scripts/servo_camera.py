2e10901ce03301df0652e7d4167b586c377f4158
Arnaldo Alves Viana Junior <arnaldo_viana@yahoo.com.br>
2019-10-21 13:50:27 -0300

    Update servo_camera.py

--- ros/exemplos_python/scripts/servo_camera.py
+++ ros/exemplos_python/scripts/servo_camera.py
@@ -1,23 +1,27 @@
 #! /usr/bin/env python
 # -*- coding:utf-8 -*-
 
-
 import rospy
 import numpy as np
 from std_msgs.msg import Float32
+
+# Os valores de posição da camera estão entre aproximadamente 4 e 8.5.
+# A posição inicial da camera é definida em pos.
 pos = 4
+
+
 
 if __name__=="__main__":
 
-	rospy.init_node("move_camera")
+    rospy.init_node("move_camera")
 
-	posicao_camera = rospy.Publisher("/servo_camera/position", Float32, queue_size = 1 )
-	
+    posicao_camera = rospy.Publisher("/servo_camera/position", Float32, queue_size = 1 )
+    
 
-	while not rospy.is_shutdown():
-		print("Oeee")
-		pos = pos + 0.5
-		posicao_camera.publish(pos)
-        if pos > 9:
+    while not rospy.is_shutdown():
+        print("teste ")
+        pos = pos + 0.5
+        posicao_camera.publish(pos)
+        if pos > = 8.5:
             pos = 4
-		rospy.sleep(2)
+        rospy.sleep(2)
