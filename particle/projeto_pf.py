8b8a9a0f5c74bc3283440985963d11f701b5fa41
Fabio de Miranda <fmiranda@gmail.com>
2020-05-28 13:20:54 -0300

    projetopf testes

--- particle/projeto_pf.py
+++ particle/projeto_pf.py
@@ -4,8 +4,7 @@
 Esta classe deve conter todas as suas implementações relevantes para seu filtro de partículas
 """
 
-
-from pf import Particle, create_particles
+from pf import Particle, create_particles, draw_random_sample
 import numpy as np
 import inspercles # necessário para o a função nb_lidar que simula o laser
 import math
@@ -21,7 +20,7 @@ robot = Particle(largura/2, altura/2, math.pi/4, 1.0)
 # Nuvem de particulas
 particulas = []
 
-num_particulas = 50
+num_particulas = 1000
 
 
 # Os angulos em que o robo simulado vai ter sensores
@@ -29,30 +28,30 @@ angles = np.linspace(0.0, 2*math.pi, num=8, endpoint=False)
 
 # Lista mais longa
 movimentos_longos = [[-10, -10, 0], [-10, 10, 0], [-10,0,0], [-10, 0, 0],
-              [0,0,math.pi/12.0], [0, 0, math.pi/12.0], [0, 0, math.pi/12],[0,0,-math.pi/4],
-              [-5, 0, 0],[-5,0,0], [-5,0,0], [-10,0,0],[-10,0,0], [-10,0,0],[-10,0,0],[-10,0,0],[-15,0,0],
-              [0,0,-math.pi/4],[0, 10, 0], [0,10,0], [0, 10, 0], [0,10,0], [0,0,math.pi/8], [0,10,0], [0,10,0], 
-              [0,10,0], [0,10,0], [0,10,0],[0,10,0],
-              [0,0,-math.radians(90)],
-              [math.cos(math.pi/3)*10, math.sin(math.pi/3),0],[math.cos(math.pi/3)*10, math.sin(math.pi/3),0],[math.cos(math.pi/3)*10, math.sin(math.pi/3),0],
-              [math.cos(math.pi/3)*10, math.sin(math.pi/3),0]]
+			  [0,0,math.pi/12.0], [0, 0, math.pi/12.0], [0, 0, math.pi/12],[0,0,-math.pi/4],
+			  [-5, 0, 0],[-5,0,0], [-5,0,0], [-10,0,0],[-10,0,0], [-10,0,0],[-10,0,0],[-10,0,0],[-15,0,0],
+			  [0,0,-math.pi/4],[0, 10, 0], [0,10,0], [0, 10, 0], [0,10,0], [0,0,math.pi/8], [0,10,0], [0,10,0], 
+			  [0,10,0], [0,10,0], [0,10,0],[0,10,0],
+			  [0,0,-math.radians(90)],
+			  [math.cos(math.pi/3)*10, math.sin(math.pi/3),0],[math.cos(math.pi/3)*10, math.sin(math.pi/3),0],[math.cos(math.pi/3)*10, math.sin(math.pi/3),0],
+			  [math.cos(math.pi/3)*10, math.sin(math.pi/3),0]]
 
 # Lista curta
 movimentos_curtos = [[-10, -10, 0], [-10, 10, 0], [-10,0,0], [-10, 0, 0]]
 
 movimentos_relativos = [[0, -math.pi/3],[10, 0],[10, 0], [10, 0], [10, 0],[15, 0],[15, 0],[15, 0],[0, -math.pi/2],[10, 0],
-                       [10,0], [10, 0], [10, 0], [10, 0], [10, 0], [10, 0],
-                       [10,0], [10, 0], [10, 0], [10, 0], [10, 0], [10, 0],
-                       [10,0], [10, 0], [10, 0], [10, 0], [10, 0], [10, 0],
-                       [0, -math.pi/2], 
-                       [10,0], [10, 0], [10, 0], [10, 0], [10, 0], [10, 0],
-                       [10,0], [10, 0], [10, 0], [10, 0], [10, 0], [10, 0],
-                       [10,0], [10, 0], [10, 0], [10, 0], [10, 0], [10, 0],
-                       [10,0], [10, 0], [10, 0], [10, 0], [10, 0], [10, 0],
-                       [0, -math.pi/2], 
-                       [10,0], [0, -math.pi/4], [10,0], [10,0], [10,0],
-                       [10,0], [10, 0], [10, 0], [10, 0], [10, 0], [10, 0],
-                       [10,0], [10, 0], [10, 0], [10, 0], [10, 0], [10, 0]]
+					   [10,0], [10, 0], [10, 0], [10, 0], [10, 0], [10, 0],
+					   [10,0], [10, 0], [10, 0], [10, 0], [10, 0], [10, 0],
+					   [10,0], [10, 0], [10, 0], [10, 0], [10, 0], [10, 0],
+					   [0, -math.pi/2], 
+					   [10,0], [10, 0], [10, 0], [10, 0], [10, 0], [10, 0],
+					   [10,0], [10, 0], [10, 0], [10, 0], [10, 0], [10, 0],
+					   [10,0], [10, 0], [10, 0], [10, 0], [10, 0], [10, 0],
+					   [10,0], [10, 0], [10, 0], [10, 0], [10, 0], [10, 0],
+					   [0, -math.pi/2], 
+					   [10,0], [0, -math.pi/4], [10,0], [10,0], [10,0],
+					   [10,0], [10, 0], [10, 0], [10, 0], [10, 0], [10, 0],
+					   [10,0], [10, 0], [10, 0], [10, 0], [10, 0], [10, 0]]
 
 
 
@@ -61,91 +60,94 @@ movimentos = movimentos_relativos
 
 
 def cria_particulas(minx=0, miny=0, maxx=largura, maxy=altura, n_particulas=num_particulas):
-    """endpointPython
-        Cria uma lista de partículas distribuídas de forma uniforme entre minx, miny, maxx e maxy
-        Angulo deve ser aleatorizado de forma uniforme entre 0 e 2pi
-    """
-    
-    particle_cloud = []
-    for i in range(n_particulas):
-        x = np.random.uniform(minx, maxx)
-        y = np.random.uniform(miny, maxy)
-        theta = np.random.uniform(0, 2*math.pi)
-        p = Particle(x, y, theta, w=1.0) # A prob. w vai ser normalizada depois
-        particle_cloud.append(p)
-    return particle_cloud    
-        
+	"""
+		Cria uma lista de partículas distribuídas de forma uniforme entre minx, miny, maxx e maxy
+	"""
+	particles_list = []
+
+	for e in range(num_particulas):
+	  x = np.random.uniform(0, largura)
+	  y = np.random.uniform(0, altura)
+	  theta = np.random.uniform(0, 2*math.pi)
+	  particle = Particle(x, y, theta, w=1/num_particulas)
+
+	  particles_list.append(particle)
+
+	return particles_list
+	
 def move_particulas(particulas, movimento):
-    """
-        Recebe um movimento na forma [deslocamento, theta]  e o aplica a todas as partículas
-        Assumindo um desvio padrão para cada um dos valores
-        Esta função não precisa devolver nada, e sim alterar as partículas recebidas.
-        
-        Sugestão: aplicar move_relative(movimento) a cada partícula
-        
-        Você não precisa mover o robô. O código fornecido pelos professores fará isso
-        
-    """
-    incerto_linear = 1
-    incerto_angular = math.radians(2)
-    
-    i_x = norm.rvs(loc = movimento[0], scale = incerto_linear)
-    i_ang = norm.rvs(loc = movimento[1], scale = incerto_angular)
-    
-    for p in particulas:
-        p.move_relative([i_x, i_ang])
-        
-    return particulas
-    
+	"""
+		Recebe um movimento na forma [deslocamento, theta]  e o aplica a todas as partículas
+		Assumindo um desvio padrão para cada um dos valores
+		Esta função não precisa devolver nada, e sim alterar as partículas recebidas.
+		
+		Sugestão: aplicar move_relative(movimento) a cada partícula
+		
+		Você não precisa mover o robô. O código fornecido pelos professores fará isso
+		
+	"""
+	movimento[0] = movimento[0] + norm.rvs(loc=0, scale=0.2)
+	movimento[1] = movimento[1] + norm.rvs(loc=0, scale=0.2)
+
+	for e in particulas:
+		e.move_relative(movimento)
+
+	return particulas
+	
 def leituras_laser_evidencias(robot, particulas):
-    """
-        Realiza leituras simuladas do laser para o robo e as particulas
-        Depois incorpora a evidência calculando
-        P(H|D) para todas as particulas
-        Lembre-se de que a formula $P(z_t | x_t) = \alpha \prod_{j}^M{e^{\frac{-(z_j - \hat{z_j})}{2\sigma^2}}}$ 
-        responde somente P(D|Hi), em que H é a hi
-        
-        Esta função não precisa retornar nada, mas as partículas precisa ter o seu w recalculado. 
-        ou
-!(2*3&-4*&|(&,$1-). !((&,$1-)
-!((&,$1-|2*3&-4*&) =
-!(2*3&-4*&)
-!(8$1-&|5+6ó,*&*). !(5+6ó,*&*)
-        Você vai precisar calcular para o robo
-        
-    """
-    
-    leitura_robo = inspercles.nb_lidar(robot, angles)
-    
-    # Voce vai precisar calcular a leitura para cada particula usando inspercles.nb_lidar e depois atualizar as probabilidades
-    for p in particulas:
-        leitura_part = inspercles.nb_lidar(p, angles)
-        for j in leitura_robo:
-            real = leitura_robo[j]
-            estimado = leitura_part[j]
-            # ponto de parada e retomada
-            
-        
+	"""
+		Realiza leituras simuladas do laser para o robo e as particulas
+		Depois incorpora a evidência calculando
+		P(H|D) para todas as particulas
+		Lembre-se de que a formula $P(z_t | x_t) = \alpha \prod_{j}^M{e^{\frac{-(z_j - \hat{z_j})}{2\sigma^2}}}$ 
+		responde somente P(D|Hi), em que H é a hi
+		
+		Esta função não precisa retornar nada, mas as partículas precisa ter o seu w recalculado. 
+		
+		Você vai precisar calcular para o robo
+		
+	"""
+	
+	leitura_robo = inspercles.nb_lidar(robot, angles)
+	scale = 10 #10cm
+	alpha = 0
+	# Voce vai precisar calcular a leitura para cada particula usando inspercles.nb_lidar e depois atualizar as probabilidades
+	for part in particulas:
+		prob = 0
+		for key, value in (inspercles.nb_lidar(part, angles)).items():
+			prob += norm.pdf(value, loc=leitura_robo[key], scale=scale)
+		part.w = prob
+		alpha += prob
 
-    
-    
+	for part in particulas:
+		part.normalize(alpha)
+
+	
+	
 def reamostrar(particulas, n_particulas = num_particulas):
-    """
-        Reamostra as partículas devolvendo novas particulas sorteadas
-        de acordo com a probabilidade e deslocadas de acordo com uma variação normal    
-        
-        O notebook como_sortear tem dicas que podem ser úteis
-        
-        Depois de reamostradas todas as partículas precisam novamente ser deixadas com probabilidade igual
-        
-        Use 1/n ou 1, não importa desde que seja a mesma
-    """
-    return particulas
+	"""
+		Reamostra as partículas devolvendo novas particulas sorteadas
+		de acordo com a probabilidade e deslocadas de acordo com uma variação normal    
+		
+		O notebook como_sortear tem dicas que podem ser úteis
+		
+		Depois de reamostradas todas as partículas precisam novamente ser deixadas com probabilidade igual
+		
+		Use 1/n ou 1, não importa desde que seja a mesma
+	"""
+	pesos = [p.w for p in particulas]
+	particulas = draw_random_sample(particulas, pesos, num_particulas)
+
+	for p in particulas:
+	    p.w = 1/num_particulas
+	    p.x += norm.rvs(loc=0, scale=10)
+	    p.y += norm.rvs(loc=0, scale=10)
+	    p.theta += norm.rvs(loc=0, scale=math.pi/20)
+
+	return particulas
 
 
-    
-
-
+	
 
 
 
