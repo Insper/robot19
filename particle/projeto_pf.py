a0928a441a020927e459de0d36644d8b38e09312
--globlal <--globlal>
2020-05-28 13:16:23 -0300

    mensagem inicial

--- particle/projeto_pf.py
+++ particle/projeto_pf.py
@@ -4,10 +4,12 @@
 Esta classe deve conter todas as suas implementações relevantes para seu filtro de partículas
 """
 
+
 from pf import Particle, create_particles
 import numpy as np
 import inspercles # necessário para o a função nb_lidar que simula o laser
 import math
+from scipy.stats import norm
 
 
 largura = 775 # largura do mapa
@@ -19,7 +21,7 @@ robot = Particle(largura/2, altura/2, math.pi/4, 1.0)
 # Nuvem de particulas
 particulas = []
 
-num_particulas = 10
+num_particulas = 50
 
 
 # Os angulos em que o robo simulado vai ter sensores
@@ -59,11 +61,20 @@ movimentos = movimentos_relativos
 
 
 def cria_particulas(minx=0, miny=0, maxx=largura, maxy=altura, n_particulas=num_particulas):
-    """
+    """endpointPython
         Cria uma lista de partículas distribuídas de forma uniforme entre minx, miny, maxx e maxy
+        Angulo deve ser aleatorizado de forma uniforme entre 0 e 2pi
     """
-    return create_particles(robot.pose())
     
+    particle_cloud = []
+    for i in range(n_particulas):
+        x = np.random.uniform(minx, maxx)
+        y = np.random.uniform(miny, maxy)
+        theta = np.random.uniform(0, 2*math.pi)
+        p = Particle(x, y, theta, w=1.0) # A prob. w vai ser normalizada depois
+        particle_cloud.append(p)
+    return particle_cloud    
+        
 def move_particulas(particulas, movimento):
     """
         Recebe um movimento na forma [deslocamento, theta]  e o aplica a todas as partículas
@@ -75,6 +86,15 @@ def move_particulas(particulas, movimento):
         Você não precisa mover o robô. O código fornecido pelos professores fará isso
         
     """
+    incerto_linear = 1
+    incerto_angular = math.radians(2)
+    
+    i_x = norm.rvs(loc = movimento[0], scale = incerto_linear)
+    i_ang = norm.rvs(loc = movimento[1], scale = incerto_angular)
+    
+    for p in particulas:
+        p.move_relative([i_x, i_ang])
+        
     return particulas
     
 def leituras_laser_evidencias(robot, particulas):
@@ -86,7 +106,11 @@ def leituras_laser_evidencias(robot, particulas):
         responde somente P(D|Hi), em que H é a hi
         
         Esta função não precisa retornar nada, mas as partículas precisa ter o seu w recalculado. 
-        
+        ou
+!(2*3&-4*&|(&,$1-). !((&,$1-)
+!((&,$1-|2*3&-4*&) =
+!(2*3&-4*&)
+!(8$1-&|5+6ó,*&*). !(5+6ó,*&*)
         Você vai precisar calcular para o robo
         
     """
@@ -94,7 +118,14 @@ def leituras_laser_evidencias(robot, particulas):
     leitura_robo = inspercles.nb_lidar(robot, angles)
     
     # Voce vai precisar calcular a leitura para cada particula usando inspercles.nb_lidar e depois atualizar as probabilidades
-
+    for p in particulas:
+        leitura_part = inspercles.nb_lidar(p, angles)
+        for j in leitura_robo:
+            real = leitura_robo[j]
+            estimado = leitura_part[j]
+            # ponto de parada e retomada
+            
+        
 
     
     
